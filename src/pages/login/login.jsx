7f8359e0918e00667a2f295a3852610120366e8d
hyq <heyingqin@chang-e.cn>
2019-10-21 18:07:35 +0800

    feat：登录静态页面

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Form, Icon, Input, Button, Checkbox } from "antd";
+import { Form, Icon, Input, Button } from "antd";
 import "./login.less";
 import logo from "./images/logo.jpg";
 /**
@@ -15,11 +15,29 @@ class Login extends Component {
     handleSubmit = e => {
         e.preventDefault();
         // 获取表单输入项
-        const value = this.props.form.getFieldsValue()
-        console.log(value)
-        // this.props.form.validateFileds(()=> {
-
-        // })
+        // const value = this.props.form.getFieldsValue()
+        
+        // 表单组件的统一验证
+        this.props.form.validateFields((err, values) => {
+            // values这个是表达数据集
+            // values和value的值相同
+            if(err) {
+                return
+            } else {
+            }
+        })
+    }
+    // 自定义验证
+    validatorPwd = (rule, value, callback) => {
+        if(!value) {
+            callback('密码必填')
+        } else if(value.length<4 || value.lengt > 12 ) {
+            callback('密码长度至少4位, 最长12位')
+        }else if(!(/^[a-zA-Z0-9_]+$/.test(value))) {
+            callback('下划线、字母和数字组合')
+        } else {
+            callback()
+        }
     }
   render() {
     const {getFieldDecorator} = this.props.form; 
@@ -36,16 +54,21 @@ class Login extends Component {
               <Item>
                 {
                     getFieldDecorator('username', {
+                        // 声明式验证：直接使用别人定义好的验证规则进行验证
                         rules: [
-                            {}
+                            { required: true, whitespace: true, message: '用户名必填'},
+                            { min: 4, message: '用户名至少4位'},
+                            { max: 12, message: '用户名最多4位'},
+                            { pattern: /^[a-zA-Z0-9_]+$/, message: '下划线、字母和数字组合'}
                         ]
-                    })(<Input prefix={<Icon type='user' style={{ color: "rgba(0,0,0,.25)"}}></Icon>}/>)
+                    })(<Input placeholder="username" prefix={<Icon type='user' style={{ color: "rgba(0,0,0,.25)"}}></Icon>}/>)
                 }
               </Item>
               <Item>
                 {getFieldDecorator("password", {
+                  // 配置对象
                   rules: [
-                    { required: true, message: "Please input your Password!" }
+                    { validator: this.validatorPwd, required: true }
                   ]
                 })(
                   <Input
@@ -98,4 +121,5 @@ export default WrappedNormalLoginForm
  * 1) 本质是一个函数
  * 2)接收一个组件（被包装的组件），返回一个新的组件（包装组件），包装组件会向被包装组件传入特定属性
  * 3)作用 扩展组件的功能
+ * 4)高阶组件也是高阶函数
  */
